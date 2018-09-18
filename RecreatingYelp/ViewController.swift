6a9209a596d511785554a26a3b2290ca252094b2
amandademetrio <amandademetrio@gmail.com>
2018-09-18 16:21:20 -0700

    Added lat long variables

--- RecreatingYelp/ViewController.swift
+++ RecreatingYelp/ViewController.swift
@@ -8,14 +8,28 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
     
     @IBOutlet var mapView: MKMapView!
     
+    var locationManager: CLLocationManager = CLLocationManager()
+    
+    var userLocation: CLLocation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.userTrackingMode = .follow
+        self.locationManager.requestAlwaysAuthorization()
+        
+        if(CLLocationManager.authorizationStatus() == .authorizedWhenInUse ||
+            CLLocationManager.authorizationStatus() ==  .authorizedAlways){
+            userLocation = locationManager.location
+            let lat = (userLocation?.coordinate.latitude)!
+            let long = (userLocation?.coordinate.longitude)!
+            print("user location is",lat,long)
+        }
     }
 
     override func didReceiveMemoryWarning() {
