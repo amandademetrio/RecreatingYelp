fadcf647f2920799c456794a083a68d047d59e63
amandademetrio <amandademetrio@gmail.com>
2018-09-18 16:11:36 -0700

    first commit

--- RecreatingYelp/ViewController.swift
+++ RecreatingYelp/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView.userTrackingMode = .follow
     }
 
     override func didReceiveMemoryWarning() {
